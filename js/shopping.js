9cfcfe85c1e0a16fc2de1500e0af5be824f2e689
Subahan <subahanislam19@gmail.com>
2022-10-15 11:23:19 +0600

    edit code

---
+++ js/shopping.js
@@ -0,0 +1,5 @@
+document.getElementById('btn-plus').addEventListener('click',function(){
+    var caseinputfield = document.getElementById('case-input-field')
+    var caseinputvalue = parseFloat(caseinputfield.value)
+    console.log(caseinputvalue)
+})
