93df28d8991cce03e5bde948748665074cd559ed
Subahan <subahanislam19@gmail.com>
2022-10-16 21:47:44 +0600

    edit code

---
+++ js/phone.js
@@ -0,0 +1,30 @@
+// start code phone 
+function phoneupdatevalue(element){
+      // input value from input function
+    var phoneinputfield = document.getElementById('phone-input-field')
+    var phoneinputvalue = parseInt(phoneinputfield.value);
+    var Newphonevalue;
+    if(element === true){
+        Newphonevalue = phoneinputvalue + 1;
+    }else{
+        Newphonevalue = phoneinputvalue -1;
+    }
+    phoneinputfield.value = Newphonevalue;
+    return Newphonevalue;
+
+}
+function phoneupdateprice( Newphonevalue){
+    // input value from price total
+    var totalphoneprice = Newphonevalue * 1219;
+    var totalelement = document.getElementById('phone-price')
+    totalelement.innerText  = totalphoneprice;
+}
+document.getElementById('phone-btn-plus').addEventListener('click',function(){
+    var  Newphonevalue = phoneupdatevalue(true)
+    phoneupdateprice( Newphonevalue)
+});
+document.getElementById('phone-btn-minus').addEventListener('click',function(){
+    var Newphonevalue = phoneupdatevalue(false)
+    phoneupdateprice( Newphonevalue)
+});
+// end code phone 
--- js/shopping.js
+++ js/shopping.js
@@ -1,5 +1,31 @@
-document.getElementById('btn-plus').addEventListener('click',function(){
-    var caseinputfield = document.getElementById('case-input-field')
-    var caseinputvalue = parseFloat(caseinputfield.value)
-    console.log(caseinputvalue)
-})
+// start code phone case
+function caseupdateinputvalue(increase){
+    // input value from input function
+  var caseinputfield = document.getElementById('case-input-field')
+  var caseinputvalue = parseInt(caseinputfield.value);
+  var Newcaseinputvalue;
+  if(increase === true ){
+    Newcaseinputvalue = caseinputvalue  + 1;
+  }else{
+    Newcaseinputvalue = caseinputvalue - 1;
+  }
+  caseinputfield.value = Newcaseinputvalue;
+  return Newcaseinputvalue;
+}
+
+function caseupdateprice(Newcaseinputvalue){
+    // input value from price total
+    var casetotalprice = Newcaseinputvalue*59;
+    var casetotalelement = document.getElementById('case-price')
+    casetotalelement.innerText = casetotalprice;
+}
+
+document.getElementById('case-btn-plus').addEventListener('click',function(){
+    var Newcaseinputvalue = caseupdateinputvalue(true)
+    caseupdateprice(Newcaseinputvalue)
+});
+document.getElementById('case-btn-minus').addEventListener('click',function(){
+    var Newcaseinputvalue = caseupdateinputvalue(false)
+    caseupdateprice(Newcaseinputvalue)
+});
+// end code phone case
